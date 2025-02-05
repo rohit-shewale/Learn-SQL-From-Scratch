92cc4ef4933396cd14a4c80a48d164a58181fc3d
Rohit K. Shewale <shewalerk2016@gmail.com>
2025-02-05 09:45:43 +0530

    Statement Commands with example

---
+++ statements.sql
@@ -0,0 +1,22 @@
+--sql statements
+
+--1)DDL
+--2)DML
+--3)TCL
+--4)DQL
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--1)DATA DEFINATION LANGUAGE[DDL]:-
+    --it is used to  database schema 
+
+    --1.1)create:
+            --it is used to create table structure 
+            create table stud_example(stud_no number, 
+                                      stud_name varchar(20),
+                                      stud_address varchar2(50),
+                                      stud_mob_no number);
+    --1.2)alter
+    --1.3)delete
+    --1.4)drop
+    --1.5)truncate
